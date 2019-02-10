2e6c35f48a30371003d35c29f7c608cf2918a414
ceilingcranes <fcspaceman@gmail.com>
2019-02-09 20:31:26 -0700

    Got liquid filling to work :)

---
+++ Assets/src/scenes/Cup.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cup : MonoBehaviour
+{
+    public float diameter;
+    public float height;
+    private float volume;
+    private Dictionary<LiquidData, float> liquidDict = new Dictionary<LiquidData, float>();
+
+    public float filledVolume = 0.0f;
+    public GameObject liquidRender;
+
+    public int testAdd = 2;
+
+    void Start()
+    {
+        volume = (float)System.Math.PI * diameter / 2 * height;
+    }
+
+    void AddLiquid(LiquidData addedLiquid, float addedVolume)
+    {
+        if (liquidDict.ContainsKey(addedLiquid))
+        {
+            liquidDict[addedLiquid] += addedVolume;
+        }
+        else
+        {
+            liquidDict[addedLiquid] = addedVolume;
+        }
+
+        filledVolume += addedVolume;
+        FixVolume();
+        UpdateLiquidRender();
+    }
+
+    void Update()
+    {
+        LiquidData test = new LiquidData("testdata");
+        AddLiquid(test, testAdd);
+    }
+
+    // TODO: Make it so that the ratios of liquids change properly here 
+    void FixVolume()
+    {
+        if (filledVolume > volume)
+        {
+            //filledVolume = volume;
+            filledVolume = 0;
+        }
+    }
+
+    private void UpdateLiquidRender()
+    {
+        // liquidRender.transform.localScale.y = filledVolume / volume;
+        Vector3 Pos = liquidRender.transform.localPosition;
+        Pos[2] = (filledVolume / volume) * 0.02f - 0.01f;
+        liquidRender.transform.localPosition = Pos;
+    }
+}
---
+++ Assets/src/scenes/LiquidData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiquidData : MonoBehaviour
+{
+    public string name = "";
+    public string color = "0x000000";
+    public float viscosity = 0.5f;
+    public LiquidData(string name)
+    {
+        name = name;
+    }
+}
