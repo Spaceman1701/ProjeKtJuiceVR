eee24dba11a1091a845cc85f15897f520194d4c1
Ethan Hunter <fc.spaceman@gmail.com>
2019-02-10 09:41:11 -0700

    text and lights and other shit

---
+++ Assets/TextBoard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextBoard : MonoBehaviour
+{
+
+    public float score;
+    public string drinkName;
+
+    public Dictionary<string, float> steps;
+
+    public TextMeshPro tm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tm = GetComponent<TextMeshPro>();
+        score = 0;
+        drinkName = "None";
+        steps = new Dictionary<string, float>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        string text =
+            "Score: " + score + "\n" +
+            "Drink: " + drinkName + "\n";
+        int currentStep = 1;
+        foreach (KeyValuePair<string, float> entry in steps)
+        {
+            string step = currentStep + ". " + entry.Key + " (" + entry.Value + " mL) \n";
+            text += step;
+        }
+
+        tm.SetText(text);
+    }
+}
