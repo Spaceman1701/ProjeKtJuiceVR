2b0c1c2fdd7211b89ec8ba7672e2fae08ad7c34e
ceilingcranes <fcspaceman@gmail.com>
2019-02-09 21:33:55 -0700

    Fuck sloshing for _real_ though ://////

--- Assets/src/scenes/Cup.cs
+++ Assets/src/scenes/Cup.cs
@@ -13,21 +13,47 @@ public class Cup : MonoBehaviour
     public GameObject liquidRender;
 
     public float testAdd = 0.5f;
+    public int difference = 25;
+    public int sloshRate = 60;
+
 
     void Start()
     {
         volume = (float)System.Math.PI * diameter / 2 * height;
     }
 
+    void Update()
+    {
+        LiquidData test = new LiquidData("testdata");
+        AddLiquid(test, testAdd);
+        Slosh();
+    }
+
     private void Slosh()
     {
         Quaternion inverseRotation = Quaternion.Inverse(transform.localRotation);
-        Vector3 liquidRotation = Quaternion.RotateTowards(liquidRender.transform.localRotation, inverseRotation, 60 * Time.deltaTime).eulerAngles;
+        Vector3 liquidRotation = Quaternion.RotateTowards(liquidRender.transform.localRotation, inverseRotation, sloshRate * Time.deltaTime).eulerAngles;
         //liquidRotation.x = ClampRotationValue(liquidRotation.x, difference);
-        //liquidRotation.y = ClampRotationValue(liquidRotation.y, difference);
-
+        // liquidRotation.y = ClampRotationValue(liquidRotation.y, difference);
+        // liquidRotation.x = liquidRotation.x - 90;
         liquidRender.transform.localEulerAngles = liquidRotation;
     }
+
+    private float ClampRotationValue(float value, float difference)
+    {
+        float returnVal = 0.0f;
+        if (value > 180)
+        {
+            returnVal = Mathf.Clamp(value, 360 - difference, 360);
+        }
+        else
+        {
+            returnVal = Mathf.Clamp(value, 0, difference);
+        }
+        return returnVal;
+
+    }
+
     void AddLiquid(LiquidData addedLiquid, float addedVolume)
     {
         if (liquidDict.ContainsKey(addedLiquid))
@@ -44,12 +70,7 @@ public class Cup : MonoBehaviour
         UpdateLiquidRender();
     }
 
-    void Update()
-    {
-        LiquidData test = new LiquidData("testdata");
-        AddLiquid(test, testAdd);
-    }
-
+    
     // TODO: Make it so that the ratios of liquids change properly here 
     void FixVolume()
     {
@@ -64,7 +85,8 @@ public class Cup : MonoBehaviour
     {
         // liquidRender.transform.localScale.y = filledVolume / volume;
         Vector3 Pos = liquidRender.transform.localPosition;
-        Pos[2] = (filledVolume / volume) * 0.02f - 0.01f;
+        //Pos[2] = (filledVolume / volume) * 0.02f - 0.01f;
+        Pos[1] = (filledVolume / volume) / 10;
         liquidRender.transform.localPosition = Pos;
     }
 }
